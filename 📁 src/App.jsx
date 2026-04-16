0d162a599c7e311217535713394ef16443c0247c
pramodj551-oss <pramodj551@gmail.com>
2026-04-16 15:14:16 +0530

    Create App.jsx

---
+++ 📁 src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import ResumeAnalyzer from "./components/ResumeAnalyzer";
+import Contact from "./components/Contact";
+import Chatbot from "./components/Chatbot";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <ResumeAnalyzer />
+      <Contact />
+      <Chatbot />
+    </>
+  );
+}
+
+export default App;
